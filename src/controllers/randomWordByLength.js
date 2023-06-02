ed4207d245b6d1ad1197f520d7d50bcb647e33dc
cahov <86586454+cahov@users.noreply.github.com>
2023-06-01 20:07:16 -0400

    Update randomWordByLength.js
    fix minor

--- src/controllers/randomWordByLength.js
+++ src/controllers/randomWordByLength.js
@@ -2,25 +2,25 @@ const fs = require('fs')
 
 const randomWordByLength = async (category, length) => {
     try {
-      const jsonData = await fs.promises.readFile('./src/files/words.json', 'utf-8');
+      const jsonData = await fs.promises.readFile('./src/files/words.json', 'utf-8')
       const data = JSON.parse(jsonData)
       let randomWord;
   
       if (category === 'all') {
         const allWords = Object.values(data).flat();
-        const wordsWithLength = allWords.filter(word => word.length === length);
+        const wordsWithLength = allWords.filter(word => word.length === length)
         if (wordsWithLength.length === 0) {
           throw new Error('No se encontro palabra con la longitud deseada')
         }
-        const randomIndex = Math.floor(Math.random() * wordsWithLength.length);
+        const randomIndex = Math.floor(Math.random() * wordsWithLength.length)
         randomWord = wordsWithLength[randomIndex]
       } else {
-        const words = data[category];
-        const wordsWithLength = words.filter(word => word.length === length);
+        const words = data[category]
+        const wordsWithLength = words.filter(word => word.length === length)
         if (wordsWithLength.length === 0) {
           throw new Error('No se encontro palabra con la longitud deseada')
         }
-        const randomIndex = Math.floor(Math.random() * wordsWithLength.length);
+        const randomIndex = Math.floor(Math.random() * wordsWithLength.length)
         randomWord = wordsWithLength[randomIndex]
       }
       return randomWord
