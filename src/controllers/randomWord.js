4a7910532a463b7db0ea116c7520ccc5d5cd3677
cahov <86586454+cahov@users.noreply.github.com>
2023-06-01 20:07:37 -0400

    Update randomWord.js
    fix minor

--- src/controllers/randomWord.js
+++ src/controllers/randomWord.js
@@ -2,16 +2,16 @@ const fs = require('fs')
 
 const randomWord = async (category) =>{
     try{
-        const jsonData = await fs.promises.readFile('./src/files/words.json', 'utf-8');
-        const data = JSON.parse(jsonData);
+        const jsonData = await fs.promises.readFile('./src/files/words.json', 'utf-8')
+        const data = JSON.parse(jsonData)
         let randomWord
         if (category=='all'){
             const allWords = Object.values(data).flat();
-            const randomIndex = Math.floor(Math.random() * allWords.length);
+            const randomIndex = Math.floor(Math.random() * allWords.length)
             randomWord = allWords[randomIndex]
         }else{
             const words = data[category]
-            const randomIndex = Math.floor(Math.random() * words.length);
+            const randomIndex = Math.floor(Math.random() * words.length)
             randomWord = words[randomIndex]
         }
         return randomWord
